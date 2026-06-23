32be4db9a16e7f1bec77ee03783e3f0856cf7961
Sirii-maker <maranatsirirat@gmail.com>
2026-06-23 14:27:48 +0900

    Create basic_queries.sql

---
+++ basic_queries.sql
@@ -0,0 +1,17 @@
+-- SELECT
+SELECT *
+FROM Customers;
+
+-- DISTINCT
+SELECT DISTINCT Country
+FROM Customers;
+
+-- WHERE
+SELECT *
+FROM Customers
+WHERE Country = 'Germany';
+
+-- ORDER BY
+SELECT *
+FROM Customers
+ORDER BY CustomerName;
