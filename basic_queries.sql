aaace97a0ec1c0a3abf7f675a540cc099f1455b0
Sirii-maker <maranatsirirat@gmail.com>
2026-06-23 15:28:31 +0900

    Update basic_queries.sql

--- basic_queries.sql
+++ basic_queries.sql
@@ -14,28 +14,3 @@ FROM Customers;
 SELECT *  -- filter records
 FROM Customers
 WHERE Country = 'Germany';
-
--- ORDER BY
-SELECT *
-FROM Customers
-ORDER BY CustomerName;
-
--- ORDER BY (ASC)
-SELECT *
-FROM Products
-ORDER BY Price;
-
--- ORDER BY (DESC)
-SELECT *
-FROM Products
-ORDER BY Price;
-
--- ORDER BY Several Columns
-SELECT *
-FROM Customers
-ORDER BY Country, CustomerName;
-
--- ORDER BY Combine ASC and DESC
-SELECT *
-FROM Customers
-ORDER BY Country ASC, CustomerName DESC;
