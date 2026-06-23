7933e28bd234706d4529091c4b061a983dff9c47
Sirii-maker <maranatsirirat@gmail.com>
2026-06-23 15:15:20 +0900

    Update basic_queries.sql

--- basic_queries.sql
+++ basic_queries.sql
@@ -2,7 +2,7 @@
 -- Topic: Basic Queries
 -- Source: W3Schools SQL Tutorial
 -- ================================
-##SELECT
+-- SELECT
 SELECT *  --select all columns
 FROM Customers;
 
@@ -19,3 +19,23 @@ WHERE Country = 'Germany';
 SELECT *
 FROM Customers
 ORDER BY CustomerName;
+
+-- ORDER BY (ASC)
+SELECT *
+FROM Products
+ORDER BY Price;
+
+-- ORDER BY (DESC)
+SELECT *
+FROM Products
+ORDER BY Price;
+
+-- ORDER BY Several Columns
+SELECT *
+FROM Customers
+ORDER BY Country, CustomerName;
+
+-- ORDER BY Combine ASC and DESC
+SELECT *
+FROM Customers
+ORDER BY Country ASC, CustomerName DESC;
