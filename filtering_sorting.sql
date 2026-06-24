3296035783f0e4823ac38df5b0bbcc75846e40b6
Sirii-maker <maranatsirirat@gmail.com>
2026-06-24 11:01:18 +0900

    Update filtering_sorting.sql

--- filtering_sorting.sql
+++ filtering_sorting.sql
@@ -22,3 +22,31 @@ ORDER BY Country, CustomerName;
 SELECT *
 FROM Customers
 ORDER BY Country ASC, CustomerName DESC;
+
+-- AND
+SELECT * 
+FROM Customers
+WHERE Country = 'Spain' AND CustomerName LIKE 'G%';
+
+-- AND --All Conditions Must Be True
+SELECT * FROM Customers
+WHERE Country = 'Brazil'
+AND City = 'Rio de Janeiro'
+AND CustomerID > 50;
+
+-- AND --Combining AND and OR
+SELECT * 
+FROM Customers
+WHERE Country = 'Spain'
+AND CustomerName LIKE 'G%' 
+OR CustomerName LIKE 'R%';
+
+-- OR
+SELECT * 
+FROM Customers
+WHERE Country = 'Germany' 
+OR Country = 'Spain';
+
+
+
+
