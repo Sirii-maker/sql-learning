4a3fbeff71c812fc99b2894f22aa6aaddf407bf0
Sirii-maker <maranatsirirat@gmail.com>
2026-06-26 14:35:49 +0900

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -1,2 +1,24 @@
--- JOIN
-SELECT
+-- ================================
+-- Topic: JOINS
+-- Source: W3Schools SQL Tutorial
+-- ================================
+
+-- INNER JOIN
+SELECT orders.OrderID, customers.customer
+FROM orders
+INNER JOIN customers ON orders.CustomerID = customers.CustomerID;
+
+-- LEFT JOIN
+SELECT orders.OrderID, customers.customer
+FROM orders
+LEFT JOIN customers ON orders.CustomerID = customers.CustomerID;
+
+-- RIGHT JOIN 
+SELECT orders.OrderID, customers.customer
+FROM orders
+RIGHT JOIN customers ON orders.CustomerID = customers.CustomerID;
+
+-- FULL JOIN
+SELECT order.OrderID, customers.customer
+FROM orders
+FULL JOIN customers ON orders.CustomerID = customer.CustomerID;
